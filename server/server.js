21392c53ed0a6f62b7fc809f0a0a514704c41635
abhishekbindraDev <35190737+abhishekbindraDev@users.noreply.github.com>
2019-01-12 13:11:18 +0530

    Greet New User and Alert Other Users

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ io.on('connection', (socket) => {
     //     createdAt: 123
     // });
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: "Welcome to the chat app",
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: "New User Joined",
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('Create Message', message);
 
@@ -23,6 +35,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         }); 
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // }); 
     });
  
     //Whenever someone disconnects this piece of code executed
